d1536db41048cc78ca2c3029a0c41563c5c2a639
Brice Boutet <boutetlb@gmail.com>
2020-07-11 22:04:59 -0400

    Navbar updated with links

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,10 +2,18 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+      <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="/books">Google Books</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="search">Search</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="saved">Saved</a>
+    </li>
+  </ul>
     </nav>
   );
 }
